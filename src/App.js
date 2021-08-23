7ec11f672291342ddd961b5fa1a9d94af43638c9
rebeccawurster <rebecca.wurster@outlook.com>
2021-08-23 23:46:44 +0200

    Added basic data and sort

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {AgGridColumn, AgGridReact} from 'ag-grid-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+const App = () => {
+   const rowData = [
+      {Typ: "Boden", Stark: "Gift, Gestein, Stahl, Feuer, Elektro", Verletzbar: "Wasser, Pflanze, Eis"},
+      {Typ: "Drache", Stark: "Drache", Verletzbar: "Eis, Drache, Fee"},
+      {Typ: "Eis", Stark: "Flug, Boden, Pflanze, Drache", Verletzbar: "Kampf, Gestein, Stahl, Feuer"},
+      {Typ: "Elektro", Stark: "Flug, Wasser", Verletzbar: "Boden"},
+      {Typ: "Fee", Stark: "Kampf, Drache, Unlicht", Verletzbar: "Gift, Stahl"},
+      {Typ: "Feuer", Stark: "Käfer, Stahl, Pflanze, Eis", Verletzbar: "Boden, Gestein, Wasser"},
+      {Typ: "Flug", Stark: "Kampf, Käfer, Pflanze", Verletzbar: "Gestein, Elektro, Eis"},
+      {Typ: "Geist", Stark: "Geist, Psycho", Verletzbar: "Geist, Unlicht"},
+      {Typ: "Gestein", Stark: "Flug, Käfer, Feuer, Eis", Verletzbar: "Kampf, Boden, Stahl, Wasser, Pflanze"},
+      {Typ: "Gift", Stark: "Pflanze, Fee", Verletzbar: "Boden, Psycho"},
+      {Typ: "Käfer", Stark: "Pflanze, Psycho, Unlicht", Verletzbar: "Flug, Gestein, Feuer"},
+      {Typ: "Kampf", Stark: "Normal, Gestein, Stahl, Eis, Unlicht", Verletzbar: "Flug, Psycho, Fee"},
+      {Typ: "Normal", Stark: "", Verletzbar: "Kampf"},
+      {Typ: "Pflanze", Stark: "Boden, Gestein, Wasser", Verletzbar: "Flug, Gift, Käfer, Feuer, Eis"},
+      {Typ: "Psycho", Stark: "Kampf, Gift", Verletzbar: "Käfer, Geist, Unlicht"},
+      {Typ: "Stahl", Stark: "Gestein, Eis, Fee", Verletzbar: "Kampf, Boden, Feuer"},
+      {Typ: "Unlicht", Stark: "Geist, Psycho", Verletzbar: "Kampf, Käfer, Fee"},
+      {Typ: "Wasser", Stark: "Boden, Gestein, Feuer", Verletzbar: "Pflanze, Elektro"}
+   ];
+
+   return (
+       <div className="ag-theme-alpine" style={{ width: '100%', height: '100%;' }}>
+           <AgGridReact 
+               rowData={rowData}>
+               <AgGridColumn field="Typ" pinned="left" lockPinned={ true } sortable={ true } filter={ true }></AgGridColumn>
+               <AgGridColumn field="Stark" headerName="Stark gegen" filter={ true }></AgGridColumn>
+               <AgGridColumn field="Verletzbar" headerName="Verletzbar von" filter={ true }></AgGridColumn>
+           </AgGridReact>
+       </div>
+   );
+};
 
 export default App;
